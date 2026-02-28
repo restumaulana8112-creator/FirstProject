cbcaed1eb4a7acb4d036a0ace71ae16dac5d1a92
Restu Maulana <restumaulana8112@gmail.com>
2026-02-28 15:16:48 +0800

    Menambah kode tlx

--- tester.cpp
+++ tester.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main() {
     cout << "Halo, Git dan C++ sudah konek!" << endl;
+
+    int jantan = 0, betina = 0;
+    int tanggal = 1;
+
+    while (betina >= jantan * 10) {
+        jantan++;
+        betina += tanggal;
+        tanggal++;
+    }
+
+    cout << tanggal << endl;
     return 0;
 }
