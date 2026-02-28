3d02056bf68ab604d62fb270a0042c3d2e199afa
Restu Maulana <restumaulana8112@gmail.com>
2026-02-28 18:02:22 +0800

    Nambah program tlx baru

--- tester.cpp
+++ tester.cpp
@@ -7,12 +7,17 @@ int main() {
     int jantan = 0, betina = 0;
     int tanggal = 1;
 
-    while (betina >= jantan * 10) {
+    while (betina <= jantan * 10) {
         jantan++;
         betina += tanggal;
         tanggal++;
     }
 
     cout << tanggal << endl;
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c << endl;
+    cout << a + b + c << endl;
     return 0;
 }
