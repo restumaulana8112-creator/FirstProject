350d9b6d4581b1ecccf851aa34829987ff2341f9
Restu Maulana <restumaulana8112@gmail.com>
2026-03-13 06:16:10 +0800

    Nambah '======' di kategori '10 Soal dari Gemini'

--- tester.cpp
+++ tester.cpp
@@ -34,7 +34,7 @@ int main() {
     }
 
     //Soal dari Gemini
-    cout << "\n 10 Soal dari Gemini \n" << "Soal Pertama:" << endl;
+    cout << "\n======10 Soal dari Gemini======\n" << "Soal Pertama:" << endl;
     for (int i = 1; i <= 5; i++) {
       for (int j = 1; j <= i; j++) {
         if (j % 2 == 0) {
@@ -120,7 +120,6 @@ int main() {
             }
         }
     }
-    
     
 
 
